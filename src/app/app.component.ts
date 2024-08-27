d3ca7db8a79934596a5ba3d19402dc5a2f0efda1
i-m-unicorn <max.dev.doc@gmail.com>
2024-08-27 11:30:51 +0300

    add project

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,20 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'project-14-version-unibell';
+
+  displayedColumns: string[] = ['id', 'name', 'track'];
+  dataSource = playList;
+
 }
+export interface PlayList {
+  id: string;
+  name: string;
+  track: string;
+  isExpanded?: boolean;
+}
+
+const playList: PlayList[] = [
+  { id: '1', name: 'Important', track: 'assets/audio/something-important.mp3', isExpanded: false },
+  { id: '2', name: 'Oslik', track: 'assets/audio/oslik.mp3', isExpanded: false },
+  { id: '2', name: 'Baby shark', track: 'assets/audio/baby_shark_sample.mp3', isExpanded: false },
+];
